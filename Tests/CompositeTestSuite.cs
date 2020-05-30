aea301dfd16ff557fc78098333a18d3ed9f59ccc
Luis Lopez <luisl@stellarscience.com>
2020-05-29 19:43:11 -0600

    Add Test Suite

---
+++ Tests/CompositeTestSuite.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+using Bonsai.Core;
+using Bonsai.Standard;
+
+namespace Tests
+{
+  public class CompositeTestSuite
+  {
+    [Test]
+    public void SequencePass()
+    {
+      BehaviourTree tree = Helper.CreateTree();
+
+      var root = Helper.CreateNode<Sequence>(tree);
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      tree.Root = root;
+
+      var result = Helper.RunBehaviourTree(tree);
+      Assert.AreEqual(BehaviourNode.Status.Success, result);
+    }
+
+    [Test]
+    public void SequenceFail()
+    {
+      BehaviourTree tree = Helper.CreateTree();
+
+      var root = Helper.CreateNode<Sequence>(tree);
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      tree.Root = root;
+
+      var result = Helper.RunBehaviourTree(tree);
+      Assert.AreEqual(BehaviourNode.Status.Failure, result);
+    }
+
+    [Test]
+    public void SelectorPass()
+    {
+      BehaviourTree tree = Helper.CreateTree();
+
+      var root = Helper.CreateNode<Selector>(tree);
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      tree.Root = root;
+
+      var result = Helper.RunBehaviourTree(tree);
+      Assert.AreEqual(BehaviourNode.Status.Success, result);
+    }
+
+    [Test]
+    public void SelectorFail()
+    {
+      BehaviourTree tree = Helper.CreateTree();
+
+      var root = Helper.CreateNode<Selector>(tree);
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      tree.Root = root;
+
+      var result = Helper.RunBehaviourTree(tree);
+      Assert.AreEqual(BehaviourNode.Status.Failure, result);
+    }
+
+    [Test]
+    public void ParallelPass()
+    {
+      BehaviourTree tree = Helper.CreateTree();
+
+      var root = Helper.CreateNode<Parallel>(tree);
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      tree.Root = root;
+
+      var result = Helper.RunBehaviourTree(tree);
+      Assert.AreEqual(BehaviourNode.Status.Success, result);
+    }
+
+    [Test]
+    public void ParallelFail()
+    {
+      BehaviourTree tree = Helper.CreateTree();
+
+      var root = Helper.CreateNode<Parallel>(tree);
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      root.AddChild(Helper.CreateNode<Fail>(tree));
+      root.AddChild(Helper.CreateNode<Success>(tree));
+      tree.Root = root;
+
+      var result = Helper.RunBehaviourTree(tree);
+      Assert.AreEqual(BehaviourNode.Status.Failure, result);
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    //[UnityTest]
+    //public IEnumerator CompositeTestSuiteWithEnumeratorPasses()
+    //{
+    //  // Use the Assert class to test conditions.
+    //  // Use yield to skip a frame.
+    //  yield return null;
+    //}
+  }
+}
---
+++ Tests/Helper.cs
@@ -0,0 +1,52 @@
+﻿
+using UnityEngine;
+using Bonsai.Core;
+
+namespace Tests
+{
+  public class Success : Task
+  {
+    public override Status Run()
+    {
+      return Status.Success;
+    }
+  }
+
+  public class Fail : Task
+  {
+    public override Status Run()
+    {
+      return Status.Failure;
+    }
+  }
+
+  public static class Helper
+  {
+    public static BehaviourNode.Status RunBehaviourTree(BehaviourTree tree)
+    {
+      tree.SortNodes();
+      tree.Start();
+
+      while (tree.IsRunning())
+      {
+        tree.Update();
+      }
+
+      return tree.LastStatus();
+    }
+
+    static public BehaviourTree CreateTree()
+    {
+      return ScriptableObject.CreateInstance<BehaviourTree>();
+    }
+
+    static public T CreateNode<T>(BehaviourTree tree) where T : BehaviourNode
+    {
+      var node = ScriptableObject.CreateInstance<T>();
+      node.Tree = tree;
+      return node;
+    }
+  }
+
+}
+

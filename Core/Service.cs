fa82a4ecd833e04e93f5baf55729eb77bb998648
Luis Lopez <luisl@stellarscience.com>
2020-06-09 21:33:46 -0500

    Add Service node.
    Service nodes can be used to update data at a specified interval.

---
+++ Core/Service.cs
@@ -0,0 +1,65 @@
+﻿
+using UnityEngine;
+using System;
+
+namespace Bonsai.Core
+{
+  public abstract class Service : Decorator
+  {
+    public float interval = 0.5f;
+    public float randomDeviation = 0f;
+    public bool restartTimerOnEnter = true;
+
+    private readonly Utility.Timer timer = new Utility.Timer();
+
+    public Action OnEvaluation = delegate { };
+
+    public sealed override void OnStart()
+    {
+      timer.OnTimeout += ServiceTick;
+      timer.AutoRestart = true;
+    }
+
+    public sealed override void OnEnter()
+    {
+      UpdateWaitTime();
+
+      if (timer.IsDone || restartTimerOnEnter)
+      {
+        timer.Start();
+      }
+
+      base.OnEnter();
+    }
+
+    public sealed override Status Run()
+    {
+      // Simply pass the status.
+      return Iterator.LastStatusReturned;
+    }
+
+    public sealed override bool CanTickOnBranch()
+    {
+      return true;
+    }
+
+    /// <summary>
+    /// Ticks the service.
+    /// </summary>
+    public sealed override void OnBranchTick()
+    {
+      timer.Update(Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Apply changes by the service
+    /// </summary>
+    protected abstract void ServiceTick();
+
+    private void UpdateWaitTime()
+    {
+      timer.WaitTime = interval + ((float)Tree.Random.NextDouble() * 2f * randomDeviation - randomDeviation);
+    }
+  }
+
+}
---
+++ Utility/Timer.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace Bonsai.Utility
+{
+
+  public class Timer
+  {
+    public float WaitTime { get; set; } = 0f;
+    public float TimeLeft { get; private set; } = 0f;
+    public bool AutoRestart { get; set; } = false;
+
+    public Action OnTimeout = delegate { };
+
+    public void Start()
+    {
+      TimeLeft = WaitTime;
+    }
+
+    public void Update(float delta)
+    {
+      if (TimeLeft > 0f)
+      {
+        TimeLeft -= delta;
+        if (IsDone)
+        {
+          OnTimeout();
+          if (AutoRestart)
+          {
+            Start();
+          }
+        }
+      }
+
+    }
+
+    public bool IsDone { get { return TimeLeft <= 0f; } }
+  }
+
+}

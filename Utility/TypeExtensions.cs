9eef4a592cec8b2efcbdfa857db2cdbe7ea42708
Luis Lopez <luisl@stellarscience.com>
2020-06-14 11:54:03 -0500

    Move type extensions to namespace to not pollute

--- Utility/TypeExtensions.cs
+++ Utility/TypeExtensions.cs
@@ -2,52 +2,55 @@
 using System;
 using System.Collections.Generic;
 
-public static class TypeExtensions
+namespace Bonsai.Utility
 {
-
-  public static readonly Dictionary<string, string> SimpleTypeNames = new Dictionary<string, string>()
+  public static class TypeExtensions
   {
-    { "Boolean", "bool"},
-    { "Int32", "int"},
-    { "Single", "float"},
-    { "Double", "double"},
-    { "String", "string"},
-  };
 
-  /// <summary>
-  /// Get the simplified, alternative name.
-  /// </summary>
-  public static string SimplifiedName(this Type type)
-  {
-    string name = type.Name;
-
-    if (name == "Object")
+    public static readonly Dictionary<string, string> SimpleTypeNames = new Dictionary<string, string>()
     {
-      name = typeof(UnityEngine.Object) == type ? "UnityObject" : "object";
-    }
-    else
+      { "Boolean", "bool"},
+      { "Int32", "int"},
+      { "Single", "float"},
+      { "Double", "double"},
+      { "String", "string"},
+    };
+
+    /// <summary>
+    /// Get the simplified, alternative name.
+    /// </summary>
+    public static string SimplifiedName(Type type)
     {
-      if (SimpleTypeNames.TryGetValue(name, out string simple))
-        name = simple;
-    }
+      string name = type.Name;
 
-    return name;
-  }
+      if (name == "Object")
+      {
+        name = typeof(UnityEngine.Object) == type ? "UnityObject" : "object";
+      }
+      else
+      {
+        if (SimpleTypeNames.TryGetValue(name, out string simple))
+          name = simple;
+      }
 
-  /// <summary>
-  /// Human-readable type name.
-  /// </summary>
-  public static string NiceName(this Type type)
-  {
-    string name = SimplifiedName(type);
-
-    // Trim off any generic type naming.
-    int genericIndex = name.IndexOf('`');
-    if (genericIndex >= 0)
-    {
-      name = name.Substring(0, genericIndex);
+      return name;
     }
 
-    return name;
+    /// <summary>
+    /// Human-readable type name.
+    /// </summary>
+    public static string NiceName(Type type)
+    {
+      string name = SimplifiedName(type);
+
+      // Trim off any generic type naming.
+      int genericIndex = name.IndexOf('`');
+      if (genericIndex >= 0)
+      {
+        name = name.Substring(0, genericIndex);
+      }
+
+      return name;
+    }
   }
 }

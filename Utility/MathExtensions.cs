995fad9d26c816783c35bc2dd3f93b05cabdb504
Luis Lopez <luisl@stellarscience.com>
2020-06-14 12:04:05 -0500

    Helper methods

---
+++ Utility/MathExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Bonsai.Utility
+{
+  public static class MathExtensions
+  {
+    public static Vector2 Round(Vector2 v)
+    {
+      return new Vector2(Mathf.Round(v.x), Mathf.Round(v.y));
+    }
+
+    public static Rect Round(Rect r)
+    {
+      return new Rect(Round(r.position), Round(r.size));
+    }
+  }
+
+}
